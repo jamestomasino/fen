bbc41eeca135dd8675530748e539001bf57d4175
James Tomasino <james@tomasino.org>
2021-11-13 18:27:05 +0000

    more tests working

--- fen_test.py
+++ fen_test.py
@@ -29,6 +29,18 @@ e4_output = "\
 ♟ ♟ ♟ ♟ . ♟ ♟ ♟\n\
 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜"
 
+# sample game 1
+pos_sample_game_1 = "r1bqkbnr/pp3p2/3p2p1/2p5/2BnP3/2N5/PPP3PP/R1BQ1RK1 w kq - 0 11"
+sample_game_1_output = "\
+♖ . ♗ ♕ ♔ ♗ ♘ ♖\n\
+♙ ♙ . . . ♙ . .\n\
+. . . ♙ . . ♙ .\n\
+. . ♙ . . . . .\n\
+. . ♝ ♘ ♟ . . .\n\
+. . ♞ . . . . .\n\
+♟ ♟ ♟ . . . ♟ ♟\n\
+♜ . ♝ ♛ . ♜ ♚ ."
+
 # Invalid formatted FEN
 pos_invalid = "rnbqkbnr/pppmpppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
 
@@ -38,6 +50,7 @@ pos_empty = ""
 def test_start():
     assert convert(pos_start) == start_output
     assert convert(pos_e4) == e4_output
+    assert convert(pos_sample_game_1) == sample_game_1_output
 
 def test_error():
     with pytest.raises(SystemExit):
