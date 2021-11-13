f50da52a5717f21c80088f66b5892ec1c4428d8e
James Tomasino <james@tomasino.org>
2021-11-13 18:19:58 +0000

    changing tests to pytest because its way easier

--- fen_test.py
+++ fen_test.py
@@ -1,4 +1,5 @@
-import unittest
+# coding=utf-8
+import pytest
 
 from fen import (
     convert,
@@ -10,17 +11,20 @@ pos_start = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 # After 1. e4
 pos_e4 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
 
-class FenTest(unittest.TestCase):
-    def test_start(self):
-        self.assertEqual(convert(pos_start), "♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖\n\
+# Invalid formatted FEN
+pos_invalid = "rnbqkbnr/pppmpppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
+
+def test_start():
+    assert convert(pos_start) == "\
+♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖\n\
 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙\n\
 . . . . . . . .\n\
 . . . . . . . .\n\
 . . . . . . . .\n\
 . . . . . . . .\n\
 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟\n\
-♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜")
+♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜"
 
-
-if __name__ == "__main__":
-    unittest.main()
+# def test_error():
+#     with pytest.raises(Exception):
+#         convert(pos_invalid)
