24958fd6ce6f960f8a0c75b4ba1a879f2bbaed96
James Tomasino <james@tomasino.org>
2021-11-13 18:07:53 +0000

    render without trailing spaces. a bit messy

--- fen.py
+++ fen.py
@@ -6,36 +6,51 @@
 import sys
 
 pieces = {
-    "p": "♙ ",
-    "r": "♖ ",
-    "n": "♘ ",
-    "b": "♗ ",
-    "q": "♕ ",
-    "k": "♔ ",
-    "P": "♟ ",
-    "R": "♜ ",
-    "N": "♞ ",
-    "B": "♝ ",
-    "Q": "♛ ",
-    "K": "♚ ",
-    "1": ". ",
-    "2": ". . ",
-    "3": ". . . ",
-    "4": ". . . . ",
-    "5": ". . . . . ",
-    "6": ". . . . . . ",
-    "7": ". . . . . . . ",
-    "8": ". . . . . . . . ",
+    "p": "♙",
+    "r": "♖",
+    "n": "♘",
+    "b": "♗",
+    "q": "♕",
+    "k": "♔",
+    "P": "♟",
+    "R": "♜",
+    "N": "♞",
+    "B": "♝",
+    "Q": "♛",
+    "K": "♚",
+    "1": ".",
+    "2": ". .",
+    "3": ". . .",
+    "4": ". . . .",
+    "5": ". . . . .",
+    "6": ". . . . . .",
+    "7": ". . . . . . .",
+    "8": ". . . . . . . .",
     "/": "\n"
 }
+validPieces = "♙♖♘♗♕♔♟♜♞♝♛♚."
+validNum = "12345678"
 
 def convert(fen):
     output = ""
+    lastChar = None
     for char in fen:
         if (char == " "):
             break
+        if (char in validNum):
+            if (lastChar != None) and (lastChar in validPieces):
+                output += " "
+            for x in range(int(char) - 1):
+               output += ". "
+            output += "."
+            lastChar = "."
         else:
-            output += pieces[char]
+            newChar = pieces[char]
+            # add spaces only between valid pieces
+            if (lastChar != None) and (lastChar in validPieces) and (newChar in validPieces):
+                output += " "
+            output += newChar
+            lastChar = newChar
     return output
 
 def main():
--- fen_test.py
+++ fen_test.py
@@ -12,14 +12,14 @@ pos_e4 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
 
 class FenTest(unittest.TestCase):
     def test_start(self):
-        self.assertEqual(convert(pos_start), "♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ \n\
-♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ \n\
-. . . . . . . . \n\
-. . . . . . . . \n\
-. . . . . . . . \n\
-. . . . . . . . \n\
-♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ \n\
-♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ ")
+        self.assertEqual(convert(pos_start), "♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖\n\
+♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙\n\
+. . . . . . . .\n\
+. . . . . . . .\n\
+. . . . . . . .\n\
+. . . . . . . .\n\
+♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟\n\
+♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜")
 
 
 if __name__ == "__main__":
